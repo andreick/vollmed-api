3a7efb4c38002ecdd21c6c3acfcdd5b82c537ca0
Andreick <andreick.gomes.ismart@gmail.com>
2022-12-28 20:28:53 -0300

    Usando paginação

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,10 +6,12 @@ import med.voll.api.medico.MedicoCreateDto;
 import med.voll.api.medico.MedicoReadDto;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -25,7 +27,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<MedicoReadDto> read() {
-        return repository.findAll().stream().map(MedicoReadDto::new).toList();
+    public Page<MedicoReadDto> read(@PageableDefault(size = 20, sort = {"nome", "crm"}, direction = Sort.Direction.DESC) Pageable pageable) {
+        return repository.findAll(pageable).map(MedicoReadDto::new);
     }
 }
